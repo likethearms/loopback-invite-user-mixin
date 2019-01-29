5b82fcfce841444e11f1338fa206e9d393e2471b
Henrik Salmela <henrik.salmela@gmail.com>
2019-01-30 00:11:43 +0200

    Invitation routes changed and create user added

--- invite-user.ts
+++ invite-user.ts
@@ -17,6 +17,12 @@ interface IEmailConfig {
 
 interface ICTX {
   emailConfig: IEmailConfig;
+  [key: string]: any;
+}
+
+interface IUser {
+  email: string;
+  companyId: string;
 }
 
 const defaultTemplateData: ITemplateData = {
@@ -28,91 +34,105 @@ const defaultTemplateData: ITemplateData = {
 };
 
 const defaultEmailOptions: IEmailConfig = {
-  redirect: 'http://localhost:3000',
+  redirect: 'http://localhost:3000/accept-invitation',
   from: 'info@test.com',
   subject: 'Invite',
   templatePath: path.resolve(__dirname, './action-email.ejs'),
 }
 
+const sendInvitationEmail = (Model, id: string, ctx: ICTX, callback: Function) => {
+  const { AccessToken, Email } = Model.app.models;
+  let member;
+  let templateData = defaultTemplateData;
+  let emailConfig = defaultEmailOptions;
+  if (ctx.emailConfig) {
+    emailConfig = {
+      ...defaultEmailOptions,
+      ...ctx.emailConfig,
+    };
+    if (ctx.emailConfig.templateData) {
+      templateData = {
+        ...defaultTemplateData,
+        ...ctx.emailConfig.templateData,
+      };
+    }
+  }
+
+  const createToken = (m) => {
+    member = m;
+    if (!m) return callback(new Error('There is no such user'));
+    return AccessToken.create({ userId: m.id });
+  };
+
+  const sendEmail = (accessToken) => {
+    const userId = member.id;
+    const accessTokenId = accessToken.id;
+
+    const url = `${emailConfig.redirect}?access_token=${accessTokenId}&user=${userId}`;
+
+    ejs.renderFile(emailConfig.templatePath, { ...templateData, url }, (ejsError, str) => {
+      if (ejsError) return callback(ejsError);
+      return Email.send({
+        from: emailConfig.from,
+        to: member.email,
+        subject: emailConfig.subject,
+        html: str,
+      }, (err) => {
+        if (err) return callback(`MailError: InvitationMail: ${err.message}`);
+        console.log('> sending password reset email to:', member.email);
+        return callback(null, member.email);
+      });
+    });
+  };
+
+  Model.findById(id)
+    .then(createToken)
+    .then(sendEmail)
+    .catch(e => callback(e));
+};
+
 module.exports = (Model) => {
-  Model.remoteMethod('invitationRequest', {
+  Model.remoteMethod('invitation', {
     accepts: [
-      {
-        arg: 'id',
-        type: 'string',
-        required: true,
-      },
+      { arg: 'data', type: 'Member', http: { source: 'body' } },
       { arg: 'req', type: 'object', http: { source: 'context' } },
     ],
     returns: [
-      {
-        arg: 'status',
-        type: 'string',
-        root: false,
-        description: 'Status of sender',
-      },
+      { arg: 'status', type: 'string', root: false, description: 'Status of sender' },
     ],
-    description: 'Send invitation email to user',
+    description: 'Create user and send invitation email to user',
     http: [
-      {
-        path: '/:id/invite',
-        verb: 'post',
-      },
+      { path: '/invite', verb: 'post', },
     ],
   });
 
-  /**
-   * resetPasswordRequest
-   * @param {Function(Error)} callback
-   */
-  Model.invitationRequest = (id: string, ctx: ICTX, callback: Function) => {
-    const { AccessToken, Email } = Model.app.models;
-    let member;
-    let templateData = defaultTemplateData;
-    let emailConfig = defaultEmailOptions;
-    if (ctx.emailConfig) {
-      emailConfig = {
-        ...defaultEmailOptions,
-        ...ctx.emailConfig,
-      };
-      if (ctx.emailConfig.templateData) {
-        templateData = {
-          ...defaultTemplateData,
-          ...ctx.emailConfig.templateData,
-        };
-      }
-    }
 
-    const createToken = (m) => {
-      member = m;
-      if (!m) return callback(new Error('There is no such user'));
-      return AccessToken.create({ userId: m.id });
-    };
+  Model.remoteMethod('sendInvitationRequest', {
+    accepts: [
+      { arg: 'id', type: 'string', required: true },
+      { arg: 'req', type: 'object', http: { source: 'context' } },
+    ],
+    returns: [
+      { arg: 'status', type: 'string', root: false, description: 'Status of sender' },
+    ],
+    description: 'Send invitation email to user',
+    http: [
+      { path: '/:id/invite-request', verb: 'post' },
+    ],
+  });
 
-    const sendEmail = (accessToken) => {
-      const userId = member.id;
-      const accessTokenId = accessToken.id;
 
-      const url = `${emailConfig.redirect}?access_token=${accessTokenId}&user=${userId}`;
+  Model.sendInvitationRequest = (id: string, ctx: ICTX, callback: Function) => {
+    sendInvitationEmail(Model, id, ctx, callback);
+  }
 
-      ejs.renderFile(emailConfig.templatePath, { ...templateData, url }, (ejsError, str) => {
-        if (ejsError) return callback(ejsError);
-        return Email.send({
-          from: emailConfig.from,
-          to: member.email,
-          subject: emailConfig.subject,
-          html: str,
-        }, (err) => {
-          if (err) return callback(`MailError: InvitationMail: ${err.message}`);
-          console.log('> sending password reset email to:', member.email);
-          return callback(null, member.email);
-        });
-      });
-    };
-
-    Model.findById(id)
-      .then(createToken)
-      .then(sendEmail)
-      .catch(e => callback(e));
+  Model.invitation = (body: IUser, ctx: ICTX, callback: Function) => {
+    Model.findById(ctx.req.accessToken.userId)
+      .then(user => {
+        body.companyId = user.companyId;
+        return Model.create(body)
+      })
+      .then(u => sendInvitationEmail(Model, u.id, ctx, callback))
+      .catch(callback);
   };
 };
